7a72578f2b15fefb66a3ec50c8eedabc8b4c348b
aleksi <aleksi.sillantaka@gmail.com>
2022-07-12 21:27:36 +0300

    Improved distribution mechanism

--- app.py
+++ app.py
@@ -1,49 +1,44 @@
 import random
 import data
 
-def data_cleaner(raw_input:list[str]) -> list[str]:
+def data_cleaner(peeps:list[str]) -> list[str]:
+    # Clean data (Capital letters and remove whitespace)
     osallistujat = []
-    for peeps in raw_input:
-        clean = peeps.replace(' ','').capitalize()
+    for peep in peeps:
+        clean = peep.replace(' ','').capitalize()
         osallistujat.append(clean)
 
     return osallistujat
 
-
-def team_generator(osallistujat:list[str]) -> dict: ### EXTRAS NEED TO BE DONE
-    #team_count = int(input("Montako paria arvotaan?: "))
-    team_count = 2
-    players_per_team = len(osallistujat) // team_count
-    extra = len(osallistujat) % team_count
-    teams = {}
-    for t in range(team_count):
-        players = []
-        for _ in range(players_per_team):
-            new_player = osallistujat.pop(osallistujat.index(random.choice(osallistujat)))
-            players.append(new_player)
-        
-        teams[t] = players
+def team_generator(osallistujat:list[str], team_count:int = 2) -> list[list[str]]:
+    """Get number of teams and divide players randomly to those teams"""
+    teams = []
+    for i in range(team_count):
+        teams.append([])
     
-    print(teams)  
+    i = 0
+    while len(osallistujat) > 0:
+        team = i % team_count
+        kilpailija = osallistujat.pop(osallistujat.index(random.choice(osallistujat)))
+        teams[team].append(kilpailija)
+        i += 1
+
     return teams
 
-
-def captains(teams_dict:dict):
-    another_dict = {}
-    keys = teams_dict.keys()
-    for key in keys:
-        kapteeni = teams_dict[key][0]
-        team_name = "Team " + kapteeni
-        another_dict[team_name] = teams_dict[key]
-    print(another_dict)        
-
+def print_teams(teams:list[list[str]]) -> None:
+    for team in teams:
+        print('Team ' + team[0])
+        for player in team:
+            print(player)
+        print('\n')
 
 def main():
     #raw_input = input("Anna osallstujat (erota pilkulla): ").split(',')
-    raw_input = data.raw_input
-    cleaned_data = data_cleaner(raw_input)
-    teams = team_generator(cleaned_data)
-    captains(teams)
+    #team_count = int(input("Montako paria arvotaan?: "))
+    team_count = 4
+    cleaned_data = data_cleaner(data.raw_input)
+    teams = team_generator(cleaned_data, team_count)
+    print_teams(teams)
 
 if __name__ == '__main__':
     main()
